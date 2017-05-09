0feb22d9b56280907f36235636cc1e8f548822b0
Leonard Lee <archerleonard@gmail.com>
2017-05-08 21:05:30 -0700

    add query, queryAll, insert, and delete function for DBHelper; Book class for Book table in DB

--- app/src/main/java/com/example/leonardlee/ebookshelf/Book.java
+++ app/src/main/java/com/example/leonardlee/ebookshelf/Book.java
@@ -1,5 +1,7 @@
 package com.example.leonardlee.ebookshelf;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -16,53 +18,70 @@ public class Book {
     private float longitude;
     private float latitude;
     private Date saveTime;
+    private SimpleDateFormat iso8601Format;
 
     public Book() {
-
+        iso8601Format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     }
 
-    public Book(String bookname, String path, int size, float longitude, float latitude, Date saveTime) {
+    public Book(String bookname, String path, int size, float longitude, float latitude, String strDate) {
+        this();
+
         this.bookname = bookname;
         this.path = path;
         this.size = size;
         this.longitude = longitude;
         this.latitude = latitude;
-        this.saveTime = saveTime;
+        this.saveTime = formatStr2DateTime(strDate);
+    }
+
+    // getter for id
+    public int getID() {
+
+        return this.id;
     }
 
     // getter & setter for name
     public void setBookname(String name) {
+
         this.bookname = name;
     }
 
     public String getBookname() {
+
         return this.bookname;
     }
 
     // getter & setter for path
     public void setPath(String path) {
+
         this.path = path;
     }
 
     public String getPath() {
+
         return this.path;
     }
 
     // getter & setter for size
     public void setSize(int size) {
+
         this.size = size;
     }
 
     public int getSize() {
+
         return this.size;
     }
 
     // getter & setter for longitude
     public void setLongitude(float longitude) {
+
         this.longitude = longitude;
     }
 
     public float getLongitude() {
+
         return this.longitude;
     }
 
@@ -80,7 +99,28 @@ public class Book {
         this.saveTime = saveTime;
     }
 
+    public void setStrDateTime(String strDateTime) {
+        this.saveTime = formatStr2DateTime(strDateTime);
+    }
+
     public Date getSaveTime() {
         return this.saveTime;
+    }
+
+    public String formatDateTime2Str() {
+        return iso8601Format.format(this.saveTime);
+    }
+
+    private Date formatStr2DateTime(String strDate) {
+        Date date = null;
+        if (strDate != null) {
+            try {
+                date = iso8601Format.parse(strDate);
+            } catch (ParseException e) {
+                date = null;
+            }
+        }
+
+        return date;
     }
 }
--- app/src/main/java/com/example/leonardlee/ebookshelf/DBHelper.java
+++ app/src/main/java/com/example/leonardlee/ebookshelf/DBHelper.java
@@ -6,57 +6,132 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leonardlee on 04/04/2017.
  */
 
 public class DBHelper extends SQLiteOpenHelper {
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "BookDB.db";
+    public static final String BOOK_TABLE_NAME = "books";
 
-    public static final String DATABASE_NAME = "ProductDB.db";
-    public static final String CONTACTS_COLUMN_ID = "id";
-    public static final String CONTACTS_TABLE_NAME = "products";
-    public static final String CONTACTS_COLUMN_NAME = "name";
-    public static final String CONTACTS_COLUMN_DESCRIPTION = "description";
-    public static final String CONTACTS_COLUMN_PRICE = "price";
-    public static final String CONTACTS_COLUMN_REVIEW = "review";
+    public static final String BOOK_COLUMN_ID = "id";
+    public static final String BOOK_COLUMN_NAME = "name";
+    public static final String BOOK_COLUMN_PATH = "path";
+    public static final String BOOK_COLUMN_SIZE = "size";
+    public static final String BOOK_COLUMN_LONGITUDE = "longitude";
+    public static final String BOOK_COLUMN_LATITUDE = "latitude";
+    public static final String BOOK_COLUMN_SAVE_TIME = "save_time";
 
     public DBHelper(Context context) {
 
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(
-            "CREATE TABLE products " +
-            "(id integer primary key, name text, description text, price text, review text)"
-        );
+        String CREATE_BOOK_TABLE_SQL = "CREATE TABLE " + BOOK_TABLE_NAME + ""
+                + BOOK_COLUMN_ID + " INTEGER PRIMARY KEY,"
+                + BOOK_COLUMN_NAME + " TEXT,"
+                + BOOK_COLUMN_PATH + " TEXT,"
+                + BOOK_COLUMN_SIZE + " INTEGER,"
+                + BOOK_COLUMN_LONGITUDE + " REAL,"
+                + BOOK_COLUMN_LATITUDE + " REAL,"
+                + BOOK_COLUMN_SAVE_TIME + " TEXT,"
+                + ")";
+        db.execSQL(CREATE_BOOK_TABLE_SQL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS products");
+        db.execSQL("DROP TABLE IF EXISTS " + BOOK_TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertContact (String name, String description, String price, String review) {
+    public void addBook (Book book) {
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("description", description);
-        contentValues.put("price", price);
-        contentValues.put("review", review);
-        db.insert("products", null, contentValues);
-        return true;
+        contentValues.put(BOOK_COLUMN_NAME, book.getBookname());
+        contentValues.put(BOOK_COLUMN_PATH, book.getPath());
+        contentValues.put(BOOK_COLUMN_SIZE, book.getSize());
+        contentValues.put(BOOK_COLUMN_LONGITUDE, book.getLongitude());
+        contentValues.put(BOOK_COLUMN_LATITUDE, book.getLatitude());
+        contentValues.put(BOOK_COLUMN_SAVE_TIME, book.formatDateTime2Str());
+
+        db.insert(BOOK_TABLE_NAME, null, contentValues);
+        db.close();
     }
 
-    public Cursor getData(String name) {
+    // query one book by id
+    public Book getBook(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "SELECT name, description, price, review "
-                                    + "FROM products "
-                                    + "WHERE name LIKE '%"+ name +"%'",
-                                    null);
-        return res;
+        String strQuerySQL = "SELECT " + BOOK_TABLE_NAME+ ", " + BOOK_COLUMN_PATH
+                            + ", " + BOOK_COLUMN_SIZE + ", " + BOOK_COLUMN_LONGITUDE
+                            + ", " + BOOK_COLUMN_LATITUDE + ", " + BOOK_COLUMN_SAVE_TIME + " "
+                            + "FROM " + BOOK_TABLE_NAME + " "
+                            + "WHERE " + BOOK_COLUMN_ID + " = " + String.valueOf(id);
+
+        Cursor cursor =  db.rawQuery( strQuerySQL, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Book book = new Book(cursor.getString(0),
+                cursor.getString(1),
+                Integer.parseInt(cursor.getString(2)),
+                Float.parseFloat(cursor.getString(3)),
+                Float.parseFloat(cursor.getString(4)),
+                cursor.getString(5)
+        );
+        return book;
     }
+
+    // query all books
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String strQuerySQL = "SELECT " + BOOK_TABLE_NAME+ ", "
+                + BOOK_COLUMN_PATH + ", "
+                + BOOK_COLUMN_SIZE + ", "
+                + BOOK_COLUMN_LONGITUDE + ", "
+                + BOOK_COLUMN_LATITUDE + ", "
+                + BOOK_COLUMN_SAVE_TIME + " "
+                + "FROM " + BOOK_TABLE_NAME + " ";
+
+        Cursor cursor = db.rawQuery(strQuerySQL, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Book book = new Book(cursor.getString(0),
+                        cursor.getString(1),
+                        Integer.parseInt(cursor.getString(2)),
+                        Float.parseFloat(cursor.getString(3)),
+                        Float.parseFloat(cursor.getString(4)),
+                        cursor.getString(5)
+                );
+                // Adding contact to list
+                books.add(book);
+            } while (cursor.moveToNext());
+        }
+        // return contact list
+        return books;
+    }
+
+    // deleting a book
+    public void deleteBook(Book book) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(BOOK_TABLE_NAME, BOOK_COLUMN_ID + " = ?",
+                new String[] {
+                        String.valueOf(book.getID())
+                }
+        );
+        db.close();
+    }
+
+
 
 }
